3ce86c42e3918800a2083d8ca269af240f73f1ae
Yurii Skoriiantov <zlojkashtan@gmail.com>
2019-02-19 02:57:33 +0200

    ++

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
+import 'slick-carousel';
+
 $( document ).ready(function() {
 
   $('.has-sub').on('click', function() {
@@ -10,5 +12,33 @@ $( document ).ready(function() {
     $('.nav-sub__button').toggleClass('d-none');
     $('body').toggleClass('is-fixed');
   });
+
+  $('.product__thumbs img').on('click', function() {
+    $('img.is-selected').removeClass('is-selected');
+    $(this).addClass('is-selected');
+    var selectedImage = $('.product__images img:eq('+ $(this).index() + ')');
+    $('html, body').animate({
+      scrollTop: $(selectedImage).offset().top
+    }, 500);
+  });
+
+
+  if ($(window).width() < 768) {
+
+    var $status = $('.js-count');
+    var $slickElement = $('.slider');
+
+    $slickElement.on('init reInit afterChange', function(event, slick, currentSlide) {
+      var i = (currentSlide ? currentSlide : 0) + 1;
+      $status.text(i + '/' + slick.slideCount);
+    });
+
+    $slickElement.slick({
+      arrows: false,
+      fade: false,
+      infinite: true
+    });
+  }
+
 
 });
